c68120f2bad4889f9c6ff3b6b8e5178686a4ae31
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-11-03 10:12:13 +0530

    Update String_Tokenizer.java

--- Java Lab Works/Programs/String_Tokenizer.java
+++ Java Lab Works/Programs/String_Tokenizer.java
@@ -1,4 +1,9 @@
- 
+ /*
+Write a Java program that reads a line of integers, 
+and then displays each integer, and the sum of all 
+the integers (Use String Tokenizer class of 
+java.util). 
+  */ 
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
