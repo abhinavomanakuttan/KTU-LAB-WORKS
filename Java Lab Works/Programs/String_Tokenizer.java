0012fe10c5f2be0fe386ea1a7507a9af59c189d6
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-11-03 10:11:24 +0530

    Add files via upload

---
+++ Java Lab Works/Programs/String_Tokenizer.java
@@ -0,0 +1,26 @@
+ 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class  String_Tokenizer  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a line of integers separated by spaces: ");
+        String inputLine = scanner.nextLine();
+        
+        // Using StringTokenizer to parse the input line
+        StringTokenizer tokenizer = new StringTokenizer(inputLine);
+        int sum = 0;
+
+        System.out.println("The integers entered are:");
+        while (tokenizer.hasMoreTokens()) {
+            // Convert each token to an integer
+            int number = Integer.parseInt(tokenizer.nextToken());
+            System.out.print(number + " ");
+            sum += number;
+        }
+        
+        System.out.println("\nThe sum of all integers is: " + sum);
+        scanner.close();
+    }
+}
