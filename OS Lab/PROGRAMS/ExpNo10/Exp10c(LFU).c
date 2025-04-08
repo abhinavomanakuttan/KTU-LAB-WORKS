afaae5a5eb6f8ca39445616925696b838c32032f
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2025-04-08 22:36:59 +0530

    Update Exp10c(LFU).c

--- OS Lab/PROGRAMS/ExpNo10/Exp10c(LFU).c
+++ OS Lab/PROGRAMS/ExpNo10/Exp10c(LFU).c
@@ -1,94 +1,65 @@
 #include <stdio.h>
-#define MAX_FRAMES 20
-#define MAX_REFERENCES 50
 
 int main() {
-    int page_references[MAX_REFERENCES], frames[MAX_FRAMES], frequency[MAX_FRAMES];
-    int num_references, num_frames, page_faults = 0;
+    int ref[50], frames[20], freq[20] = {0};
+    int n, f, faults = 0;
 
-    // Input: Number of page references
     printf("Enter number of page references: ");
-    scanf("%d", &num_references);
-
-    // Input: Page reference sequence
-    printf("Enter the page references: ");
-    for (int i = 0; i < num_references; i++) {
-        scanf("%d", &page_references[i]);
+    scanf("%d", &n);
+    
+    printf("Enter page references: ");
+    for(int i=0; i<n; i++) {
+        scanf("%d", &ref[i]);
     }
 
-    // Input: Number of frames
-    printf("Enter the number of frames: ");
-    scanf("%d", &num_frames);
+    printf("Enter number of frames: ");
+    scanf("%d", &f);
 
-    // Initialize frames and frequency counters
-    for (int i = 0; i < num_frames; i++) {
-        frames[i] = -1;       // -1 indicates empty frame
-        frequency[i] = 0;     // Initialize usage frequency
+    // Initialize all frames as empty (-1)
+    for(int i=0; i<f; i++) {
+        frames[i] = -1;
     }
 
-    printf("\nLFU Page Replacement Algorithm\n");
-    printf("Page\tFrames\t\tPage Fault\n");
+    printf("\nPage\tFrames\t\tFault\n");
 
-    for (int i = 0; i < num_references; i++) {
-        int current_page = page_references[i];
-        int page_found = 0;
+    for(int i=0; i<n; i++) {
+        int page = ref[i];
+        int found = 0;
 
-        // Check if page is already in a frame
-        for (int j = 0; j < num_frames; j++) {
-            if (frames[j] == current_page) {
-                page_found = 1;
-                frequency[j]++;  // Increment usage count
+        // Check if page is already in frames
+        for(int j=0; j<f; j++) {
+            if(frames[j] == page) {
+                found = 1;
+                freq[j]++;
                 break;
             }
         }
 
-        // Page fault handling
-        if (!page_found) {
-            // Check for empty frame first
-            int empty_frame = -1;
-            for (int j = 0; j < num_frames; j++) {
-                if (frames[j] == -1) {
-                    empty_frame = j;
-                    break;
+        // If page not found, replace LFU page
+        if(!found) {
+            // Find least frequently used page
+            int lfu = 0;
+            for(int j=1; j<f; j++) {
+                if(freq[j] < freq[lfu]) {
+                    lfu = j;
                 }
             }
-
-            if (empty_frame != -1) {
-                // Use empty frame
-                frames[empty_frame] = current_page;
-                frequency[empty_frame] = 1;
-            } else {
-                // Find LFU page
-                int lfu_index = 0;
-                for (int j = 1; j < num_frames; j++) {
-                    if (frequency[j] < frequency[lfu_index]) {
-                        lfu_index = j;
-                    }
-                }
-
-                // Replace LFU page
-                frames[lfu_index] = current_page;
-                frequency[lfu_index] = 1;
-            }
-            page_faults++;
+            
+            // Replace
+            frames[lfu] = page;
+            freq[lfu] = 1;
+            faults++;
         }
 
         // Display current state
-        printf("%d\t", current_page);
-        for (int j = 0; j < num_frames; j++) {
-            if (frames[j] == -1) {
-                printf("-\t");
-            } else {
-                printf("%d(%d)\t", frames[j], frequency[j]);
-            }
+        printf("%d\t", page);
+        for(int j=0; j<f; j++) {
+            if(frames[j] == -1) printf("- ");
+            else printf("%d(%d) ", frames[j], freq[j]);
         }
-
-        if (!page_found) {
-            printf("PF %d", page_faults);
-        }
-        printf("\n");
+        printf("\t%s\n", found ? "" : "Fault");
     }
 
-    printf("\nTotal number of page faults: %d\n", page_faults);
+    printf("\nTotal page faults: %d\n", faults);
     return 0;
 }
